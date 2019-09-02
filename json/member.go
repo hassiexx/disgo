060f41ad72c610efacabc824fdcba5b442d8663e
hassieswift621 <hassie.dev@gmail.com>
2019-09-02 11:12:36 +0100

    Member JSON

---
+++ json/member.go
@@ -0,0 +1,12 @@
+package json
+
+// Member is a struct for a member.
+type Member struct {
+	Deaf         bool     `json:"deaf"`
+	JoinedAt     string   `json:"joined_at"`
+	Mute         bool     `json:"mute"`
+	Nick         string   `json:"nick"`
+	PremiumSince string   `json:"premium_since"`
+	Roles        []string `json:"roles"`
+	User         *User    `json:"user"`
+}
