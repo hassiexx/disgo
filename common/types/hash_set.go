09cc18d297bfe1acd8db8ba8ac43ad94763a8034
hassieswift621 <hassie.dev@gmail.com>
2019-09-07 18:06:30 +0100

    More methods for hash set

--- common/types/hash_set.go
+++ common/types/hash_set.go
@@ -1,21 +1,34 @@
 package types
 
-type setValue struct{}
+// HashSetValue is an empty hash set value struct.
+type hashSetValue struct{}
 
 // StringHashSet is a type for a string hash set.
 type StringHashSet struct {
-	m map[string]setValue
+	m map[string]hashSetValue
+}
+
+// NewStringHashSet creates a new string hash set.
+func NewStringHashSet() *StringHashSet {
+	return &StringHashSet{
+		m: make(map[string]hashSetValue),
+	}
 }
 
 // Add adds a value to the hash set.
 func (s *StringHashSet) Add(v string) {
-	s.m[v] = setValue{}
+	s.m[v] = hashSetValue{}
 }
 
 // Contains checks whether the specified value is in the hash set.
 func (s *StringHashSet) Contains(v string) bool {
 	_, exists := s.m[v]
 	return exists
+}
+
+// Empty checks whether the hash set is empty.
+func (s *StringHashSet) Empty() bool {
+	return len(s.m) == 0
 }
 
 // Remove removes a value from the hash set.
