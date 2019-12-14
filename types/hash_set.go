51259acf7a9109a97926e12124d208a5f046e010
hassieswift621 <hey@hassie.ninja>
2019-12-14 16:32:15 +0000

    Update hash set

--- types/hash_set.go
+++ types/hash_set.go
@@ -1,23 +1,20 @@
 package types
 
-// HashSetValue is an empty hash set value struct.
-type hashSetValue struct{}
-
 // StringHashSet is a type for a string hash set.
 type StringHashSet struct {
-	m map[string]hashSetValue
+	m map[string]struct{}
 }
 
 // NewStringHashSet creates a new string hash set.
 func NewStringHashSet() *StringHashSet {
 	return &StringHashSet{
-		m: make(map[string]hashSetValue),
+		m: make(map[string]struct{}),
 	}
 }
 
 // Add adds a value to the hash set.
 func (s *StringHashSet) Add(v string) {
-	s.m[v] = hashSetValue{}
+	s.m[v] = struct{}{}
 }
 
 // Contains checks whether the specified value is in the hash set.
@@ -47,19 +44,19 @@ func (s *StringHashSet) Values() []string {
 
 // UInt64HashSet is a type for a uint64 hash set.
 type UInt64HashSet struct {
-	m map[uint64]*hashSetValue
+	m map[uint64]struct{}
 }
 
 // NewUInt64HashSet creates a new string hash set.
 func NewUInt64HashSet() *UInt64HashSet {
 	return &UInt64HashSet{
-		m: make(map[uint64]*hashSetValue),
+		m: make(map[uint64]struct{}),
 	}
 }
 
 // Add adds a value to the hash set.
 func (s *UInt64HashSet) Add(v uint64) {
-	s.m[v] = nil
+	s.m[v] = struct{}{}
 }
 
 // Contains checks whether the specified value is in the hash set.
